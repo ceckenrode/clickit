d5127bc5d8d94043689c4d7652d0677ee0b5ad06
Rolando Cosme <rolandocosme@Rolandos-MBP.home>
2015-11-15 19:26:15 -0500

    added js functions related to timer button

---
+++ js/timer.js
@@ -0,0 +1,22 @@
+var = secondsCount = 0;
+var = timerInterval;
+var = toggleStart = document.getElementById( "startbtn" );
+
+
+setTimeout ( function() {
+  toggleStart.parentNode.removeChild(toggleStart);
+  alert("hurry up fast!")
+}, 3000 ) ;
+
+
+function addSeconds(){
+  secondsCount++;
+}
+
+
+function toggleTime() {
+  alert(yes!);
+}
+
+
+toggleStart.addEventListener("click" , toggleTime);
